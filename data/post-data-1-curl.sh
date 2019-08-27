2e50daf06a245d417f4180560993c2d4dc7cdd45
zouheircadi <zouheir.cadi@gmail.com>
2019-08-27 20:51:44 +0200

    adapt script to v7

--- data/post-data-1-curl.sh
+++ data/post-data-1-curl.sh
@@ -5,8 +5,6 @@ curl -XPUT "http://localhost:9200/hol_devoxxfr_pm1" -H 'Content-Type: applicatio
 {
   "mappings": 
   {
-    "_doc" :
-    {
       "properties" : 
       {
         "code" : 
@@ -18,11 +16,10 @@ curl -XPUT "http://localhost:9200/hol_devoxxfr_pm1" -H 'Content-Type: applicatio
           "type" : "keyword"
         }
       }
-    }
   }
 }'
 
-curl -XPOST "http://localhost:9200/hol_devoxxfr_pm1/_doc/_bulk" -H 'Content-Type: application/json' -d'
+curl -XPOST "http://localhost:9200/hol_devoxxfr_pm1/_bulk" -H 'Content-Type: application/json' -d'
 { "index": {  }}
 {"code":"AFG","country":"Afghanistan"}
 { "index": {  }}
