f80bdf0aed4152603708667bec3d30c5f676ffe3
zouheircadi <zouheir.cadi@gmail.com>
2020-06-19 16:19:44 +0200

    homogenize code for all ops branches

--- data/create-100000-lines-file.sh
+++ data/create-100000-lines-file.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sed -n 1,10001p tp-elastic/data/googleplaystore.csv > tp-elastic/data/googleplaystore.100000.csv
+sed -n 1,10001p googleplaystore.csv > googleplaystore.100000.csv
 
 for i in {1..9}
 do
-   sed -n 2,10001p tp-elastic/data/googleplaystore.csv >> tp-elastic/data/googleplaystore.100000.csv
+   sed -n 2,10001p googleplaystore.csv >> googleplaystore.100000.csv
 done
 
